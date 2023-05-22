e83db913f728bf1706cc9d52c58d2f01ea7e6858
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-05-22 21:56:11 +0300

    good

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
-
+const { NODE_ENV } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://0.0.0.0:27017/bitfilmsdb');
+if (NODE_ENV === 'production') {
+  mongoose.connect('mongodb://0.0.0.0:27017/bitfilmsdb');
+} else {
+  mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+}
 
 app.use(express.json());
 
