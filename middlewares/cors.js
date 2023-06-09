318bdd7e103885fb168daf3b867be9fc1ddb4da8
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-06-09 22:02:41 +0300

    1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,9 +2,9 @@ const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'localhost:3000',
+  'https://localhost:3000',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  /(https|http)?:\/\/(?:www\.|(?!www))front-movies.nomoredomains.xyz\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
 ];
 
 module.exports = (req, res, next) => {
